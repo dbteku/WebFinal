23ca5cc74b342cacda3c388132e786107ee503af
Selina744 <selina744@gmail.com>
2015-03-12 23:46:59 -0600

    pic slideshow

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var counter = 1;
+    var pic = document.getElementById("pic");
+    
+    
+function changePicture(){
+
+    if(counter >= 3){
+        counter = 1;
+    }
+    else{
+        counter++;
+    }
+    switch(counter){
+    case 1:
+          pic.setAttribute("src", "images/racetrack.jpg");  
+        break;
+    case 2:
+          pic.setAttribute("src", "images/maxresdefault.jpg");  
+        break;
+    case 3:
+        pic.setAttribute("src", "images/HotWheelsTrack.jpg");
+        break;
+    }
+}
+    setInterval(changePicture, 3000);
+});
+
+
+
+    
