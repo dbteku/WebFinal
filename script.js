0b06e3410deeec1ca41003c4f732c3521d761091
Selina744 <selina744@gmail.com>
2015-03-16 08:40:54 -0600

    buttons

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
     var counter = 1;
     var pic = document.getElementById("pic");
     
+    $(".hover").on("mouseover", function(){
+        $(this).addClass("highlight");
+    });
+     $(".hover").on("mouseout", function(){
+        $(this).removeClass("highlight");
+    });
+    
     
 function changePicture(){
     if(counter >= 3){
